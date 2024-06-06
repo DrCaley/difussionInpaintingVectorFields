ee8a234357217473534b74fe767fd5d584b66614
Ember <mcewenog@plu.edu>
2024-06-06 15:28:09 -0700

    modified the dataloader to use tensors instead of pngs

--- dataloader.py
+++ dataloader.py
@@ -5,38 +5,40 @@ from PIL import Image
 from scipy.io import loadmat
 from torch.utils.data import Dataset, DataLoader
 from image_noiser import generate_noised_images
+import os
 import numpy as np
 import datetime
 
 
 class OceanImageDataset(Dataset):
-    def __init__(self, mat_file, boundaries, img_dir):
+    def __init__(self, mat_file, boundaries, tensor_dir):
         self.mat_data = loadmat(mat_file)
-        self.img_labels = []
+        self.tensor_labels = []
+        self.tensor_arr = []
 
         with open(boundaries, 'r') as file:
             self.boundaries = yaml.safe_load(file)
 
-        self.images = []
-
         for x in range(5):
-            # change to load_array
-            self.generate_image(x)
+            self.load_array(x)
+
+        for dirpath, dirnames, filenames in os.walk(tensor_dir):
+            for filename in filenames:
+                self.tensor_arr.append(torch.load(dirpath + filename))
+                self.tensor_labels.append(filename)
+
+        print('done')
 
     def __len__(self):
-        return len(self.images)
+        return len(self.tensor_arr)
 
     def __getitem__(self, idx):
-        image = self.images[idx]
-        img_label = self.img_labels[idx]
-        transform = transforms.Compose([transforms.PILToTensor()])
 
-        if isinstance(image, Image.Image):
-            vector = transform(image)
+        tensor = self.tensor_arr[idx]
+        tensor_label = self.tensor_labels[idx]
+        return tensor, tensor_label
 
-        return vector, img_label
-
-    def load_array(self, image_num):
+    def load_array(self, tensor_num):
         u_array = self.mat_data['u']
         v_array = self.mat_data['v']
         time = self.mat_data['ocean_time'].squeeze()
@@ -46,58 +48,33 @@ class OceanImageDataset(Dataset):
         minV, maxV = -0.6647028130174489, 0.5259408400292674
 
         # Normalize and multiply by 255
-        adjusted_u_arr = 255 * (u_array - minU) / (maxU - minU)
-        adjusted_v_arr = 255 * (v_array - minV) / (maxV - minV)
+        adjusted_u_arr = (u_array - minU) / (maxU - minU)
+        adjusted_v_arr = (v_array - minV) / (maxV - minV)
 
-        for y in range(94):
-            for x in range(44):
-                pass
+        #Convert to tensors
+        u_tensors = torch.from_numpy(adjusted_u_arr)
+        u_tensors = u_tensors.permute(*torch.arange(u_tensors.ndim - 1, -1, -1))[tensor_num]
+        v_tensors = torch.from_numpy(adjusted_v_arr)
+        v_tensors = v_tensors.permute(*torch.arange(v_tensors.ndim - 1, -1, -1))[tensor_num]
 
-    def generate_image(self, image_num, img_dir):
-        """Generates images"""
-        img = Image.new('RGB', (94, 44), color='white')
+        land_tensors = u_tensors.clone().detach()
 
-        u_array = self.mat_data['u']
-        v_array = self.mat_data['v']
-        time = self.mat_data['ocean_time'].squeeze()
-        time_array = [
-            datetime.datetime.fromordinal(int(t)) + datetime.timedelta(days=t % 1) - datetime.timedelta(days=366) for t
-            in time]
-
-        # Previously found using the normal method
-        minU, maxU = -0.8973235906436031, 1.0859991093945718
-        minV, maxV = -0.6647028130174489, 0.5259408400292674
-
-        # Normalize and multiply by 255
-        adjusted_u_arr = 255 * (u_array - minU) / (maxU - minU)
-        adjusted_v_arr = 255 * (v_array - minV) / (maxV - minV)
-
-        for y in range(94):
-            for x in range(44):
-                isLand = False
-                if np.isnan(adjusted_u_arr[y][x][image_num]):  # When there was no u value
-                    curr_u = 0
-                    isLand = True  # The minimal dataloader displayed it is land
+        #1 if land, 0 if water
+        for x in range(94):
+            for y in range(44):
+                if land_tensors[y][x].isnan():
+                    land_tensors[y][x] = 1
                 else:
-                    curr_u = int(adjusted_u_arr[y][x][image_num])
-                if np.isnan(adjusted_v_arr[y][x][0]):  # When there was no v value
-                    curr_v = 0  # The minimal dataloader displayed it as a place in the ocean with no current
-                    curr_u = 0
-                else:
-                    curr_v = int(adjusted_v_arr[y][x][image_num])
+                    land_tensors[y][x] = 0
 
-                img.putpixel((y, 43 - x), (curr_u, curr_v, isLand * 255))
-
-        self.images.append(img)
-        self.img_labels.append(image_num)
-        # img.save(os.path.join(img_dir, 'ocean_image' + str(image_num) + '.png'), 'PNG')
-        # return img
+        combined_tensor = torch.stack((u_tensors, v_tensors, land_tensors))
+        torch.save(combined_tensor, "./data/tensors/" + str(tensor_num) + ".pt")
 
 
 training_data = OceanImageDataset(
     mat_file="./data/rams_head/stjohn_hourly_5m_velocity_ramhead_v2.mat",
     boundaries="./data/rams_head/boundaries.yaml",
-    img_dir="./data/images"
+    tensor_dir="./data/tensors/"
 )
 
 train_loader = DataLoader(
@@ -113,3 +90,4 @@ for epoch in range(1):
         tensor, label = data
         # apply noise to each img
         noised_tensor = torch.from_numpy(generate_noised_images(tensor))
+        print(noised_tensor)
