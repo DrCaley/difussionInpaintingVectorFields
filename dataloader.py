745b06a45cbd3e985e79316369dc33f134a69a5c
ashleyakamine <ashley.akamine@plu.edu>
2024-06-11 15:46:17 -0700

    added yolo_net and changed dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,11 @@
 import torch
-import torchvision.transforms as transforms
 import yaml
-from PIL import Image
 from scipy.io import loadmat
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
+
 
 class OceanImageDataset(Dataset):
-    def __init__(self, mat_file, boundaries, tensor_dir):
+    def __init__(self, mat_file, boundaries, num):
         self.mat_data = loadmat(mat_file)
         self.tensor_labels = []
         self.tensor_arr = []
@@ -14,24 +13,16 @@ class OceanImageDataset(Dataset):
         with open(boundaries, 'r') as file:
             self.boundaries = yaml.safe_load(file)
 
-        for x in range(5):
+        for x in range(num):
             self.tensor_arr.append(self.load_array(x))
-
-        # for dirpath, dirnames, filenames in os.walk(tensor_dir):
-        #     for filename in filenames:
-        #         self.tensor_arr.append(torch.load(dirpath + filename))
-        #         self.tensor_labels.append(filename)
-
-        print('done')
 
     def __len__(self):
         return len(self.tensor_arr)
 
     def __getitem__(self, idx):
         tensor = self.tensor_arr[idx]
-
-        target = self.tensor_labels
-        return tensor, target
+        label = self.tensor_labels
+        return tensor, label
 
     def load_array(self, tensor_num):
         u_array = self.mat_data['u']
@@ -46,7 +37,7 @@ class OceanImageDataset(Dataset):
         adjusted_u_arr = (u_array - minU) / (maxU - minU)
         adjusted_v_arr = (v_array - minV) / (maxV - minV)
 
-        #Convert to tensors
+        # Convert to tensors
         u_tensors = torch.from_numpy(adjusted_u_arr)
         u_tensors = u_tensors.permute(*torch.arange(u_tensors.ndim - 1, -1, -1))[tensor_num]
         v_tensors = torch.from_numpy(adjusted_v_arr)
@@ -54,7 +45,7 @@ class OceanImageDataset(Dataset):
 
         land_tensors = u_tensors.clone().detach()
 
-        #1 if land, 0 if water
+        # 1 if land, 0 if water
         for x in range(94):
             for y in range(44):
                 if land_tensors[y][x].isnan() or v_tensors[y][x].isnan() or u_tensors[y][x].isnan():
@@ -65,6 +56,12 @@ class OceanImageDataset(Dataset):
                     land_tensors[y][x] = 0
 
         combined_tensor = torch.stack((u_tensors, v_tensors, land_tensors))
-        # torch.save(combined_tensor, "./data/tensors/" + str(tensor_num) + ".pt")
         return combined_tensor
 
+# dataset = OceanImageDataset(
+#     mat_file="./data/rams_head/stjohn_hourly_5m_velocity_ramhead_v2.mat",
+#     boundaries="./data/rams_head/boundaries.yaml",
+#     tensor_dir="./data/tensors/"
+# )
+#
+# train_data, test_data, holdout_data = random_split(dataset, [7, 2, 1])
