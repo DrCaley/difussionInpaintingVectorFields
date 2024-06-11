dd5eaafdc5dd9f1d5b225f0f695992fcb6d3054c
Ember <mcewenog@plu.edu>
2024-06-11 12:19:49 -0700

    feat: made resize_tensor work for 3xAnyxAny tensors

--- resize_tensor.py
+++ resize_tensor.py
@@ -1,12 +1,13 @@
 import torch
 from tensors_to_png import generate_png
-#input should be a 3x44x94 tensor and a tuple of the desired demensions for the last two
+
+
+#input should be a 3xAnyXAny tensor and a tuple of the desired dimensions for the last two
 #outputs are normalized, so comparing the strength of currents between maps won't work
 def resize(tensors, end_shape):
-    expected_shape = (3, 44, 94)
     actual_shape = tensors.shape
-    if actual_shape != expected_shape:
-        raise ValueError(f"Expected tensor with shape {expected_shape}, but got {actual_shape}.")
+    if actual_shape[0] != 3:
+        raise ValueError(f"Expected tensor with shape (3, Any, Any), but got {actual_shape}.")
 
     #1st dimension
     zeros_tensor = torch.zeros((actual_shape[0], end_shape[1] - actual_shape[1], actual_shape[2]) )
@@ -18,4 +19,4 @@ def resize(tensors, end_shape):
     return resized_tensor
 
 
-generate_png(resize(torch.load("./data/tensors/0.pt"), (3, 512, 512)), )
+#generate_png(resize(torch.load("./data/tensors/0.pt"), (3, 512, 512)), )
