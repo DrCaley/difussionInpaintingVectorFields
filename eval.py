787c47051b42c72dd920c6e232eec5f0a7056519
ashleyakamine <ashley.akamine@plu.edu>
2024-06-13 13:30:13 -0700

    working code on server

---
+++ eval.py
@@ -0,0 +1,39 @@
+import torch
+from torchvision.utils import make_grid
+from torchvision.utils import save_image
+from resize_tensor import resize
+from image_noiser import generate_noised_tensor_single_step, generate_noised_tensor_iterative
+from random import randint
+import time
+
+def unnormalize(tensor, mean, std):
+    mean = torch.tensor(mean).clone().detach().view(1, -1, 1, 1)
+    std = torch.tensor(std).clone().detach().view(1, -1, 1, 1)
+    return tensor * std + mean
+
+
+def evaluate(model, testloader, device):
+    model.eval()
+    tot_loss = 0
+    with torch.no_grad():
+        for i, (tensor, label) in enumerate(testloader):
+            tensor = tensor.float().to(device)
+            mask = (tensor != 0).float().to(device)
+
+            target = generate_noised_tensor_single_step(tensor, target_iteration=randint(1, 1000),
+                                                        var_per_iteration=0.005).float().to(device)
+            tensor = generate_noised_tensor_iterative(target, iteration=1, variance=0.005).float().to(device)
+
+            tensor = resize(tensor, (3, 512, 512)).to(device)
+            target = resize(target, (3, 512, 512)).to(device)
+            mask = resize(mask, (3, 512, 512)).to(device)
+
+            output = model(tensor, mask)
+
+            output = output * mask
+            target = target * mask
+            loss = ((output - target) ** 2 * mask).sum() / mask.sum()
+            tot_loss += loss
+
+    avg_loss = tot_loss / len(testloader)
+    return avg_loss
--- yolo_train.py
+++ yolo_train.py
@@ -2,48 +2,27 @@ import torch
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 from random import randint
-
-from dataloader import OceanImageDataset
-from example_nn.evaluation import evaluate
 import math
+from dataloader import OceanImageDataset
 from example_nn.inPaintingNetwork import Net
-
 from resize_tensor import resize
 from image_noiser import generate_noised_tensor_single_step, generate_noised_tensor_iterative
-from tensors_to_png import generate_png
+from eval import evaluate
 
-
-class Args:
-    lr = 0.001
-    lr_finetune = 0.0001
-    resume = None
-    max_iter = 10000
-    log_interval = 100
-    save_model_interval = 1000
-    vis_interval = 500
-    save_dir = "./checkpoints"
-    finetune = False
-    LAMBDA_DICT = {
-        'valid': 1.0,
-        'hole': 6.0,
-        'prc': 0.05,
-        'style': 120.0,
-        'tv': 0.1
-    }
-
-
-args = Args()
+lr = 0.001
+max_iter = 100
 
 data = OceanImageDataset(
     mat_file="./data/rams_head/stjohn_hourly_5m_velocity_ramhead_v2.mat",
     boundaries="./data/rams_head/boundaries.yaml",
-    num=10
+    num=1000
 )
 
 train_len = int(math.floor(len(data) * 0.7))
 test_len = int(math.floor(len(data) * 0.15))
 val_len = len(data) - train_len - test_len
 
+torch.manual_seed(42)
 training_data, test_data, validation_data = random_split(data, [train_len, test_len, val_len])
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -52,51 +31,40 @@ train_loader = DataLoader(training_data, batch_size=1, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=1)
 val_loader = DataLoader(validation_data, batch_size=1)
 
-print(f"Number of training samples: {len(training_data)}")
-
 model = Net().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-
-start_iter = 0
-num_epochs = args.max_iter // len(train_loader) + 1
+num_epochs = max_iter // len(train_loader) + 1
 
 for epoch in range(num_epochs):
     model.train()
     for i, (tensor, label) in enumerate(tqdm(train_loader)):
         tensor = tensor.float()
-
-        # Generate mask (assuming the mask is 1 where the data is missing and 0 elsewhere)
         mask = (tensor != 0).float()
 
         target = generate_noised_tensor_single_step(tensor, target_iteration=randint(1, 1000),
-                                                    var_per_iteration=0.005).float()
-        tensor = generate_noised_tensor_iterative(target, iteration=1, variance=0.005).float()
+                                                    var_per_iteration=0.005).float().to(device)
+        tensor = generate_noised_tensor_iterative(target, iteration=1, variance=0.005).float().to(device)
 
-        tensor = resize(tensor, (3, 512, 512))
-        target = resize(target, (3, 512, 512))
-        mask = resize(mask, (3, 512, 512))
-        # Forward pass through the model
+        tensor = resize(tensor, (3, 512, 512)).to(device)
+        target = resize(target, (3, 512, 512)).to(device)
+        mask = resize(mask, (3, 512, 512)).to(device)
+
         output = model(tensor, mask)
-
-        #calculate loss
         output = output * mask
         target = target * mask
-        squared_error = (output - target) ** 2
-        masked_squared_error = squared_error * mask
-        loss = masked_squared_error.sum() / mask.sum()
+        loss = ((output - target) ** 2 * mask).sum() / mask.sum()
 
         if i % 100 == 0:
-            print(loss.item())
+            print(f"Epoch [{epoch}/{num_epochs}], Step [{i}/{len(train_loader)}], Loss: {loss.item()}")
 
-        # Backpropagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if (epoch * len(train_loader) + i + 1) % args.vis_interval == 0:
+        if (epoch * len(train_loader) + i + 1) % 1 == 0:
             model.eval()
-            evaluate(model, test_loader, device,
-                     '{:s}/images/test_{:d}.jpg'.format(args.save_dir, epoch * len(train_loader) + i + 1))
-
-
+            avg_loss = evaluate(model, test_loader, device)
+            print(f"Evaluation at {str((epoch * len(train_loader) + i + 1) % 1)}:"
+                  f"{avg_loss}")
+            model.train()
