a889def3ce94cb8dfb2e14b7233201e2e88f5479
ashleyakamine <ashley.akamine@plu.edu>
2024-06-27 12:25:38 -0700

    added new directories

--- scripts/yolo/yolo_sample_2.py
+++ scripts/yolo/yolo_sample_2.py
@@ -21,14 +21,12 @@ test_dataloader = DataLoader(test_data, batch_size=15, shuffle=True)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = Net().to(device)
-model_path = 'models/mnist_model_ep_1.pth'
+model_path = '../../models/mnist_model_ep_1.pth'
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.eval()
 
-
 T = 100
 use_known_samples = False
-
 
 def generate_samples(model, T, device, noised_samples, mask):
     sample = noised_samples.clone().float().to(device)
@@ -64,27 +62,28 @@ def get_known_samples(tensor, num_known_points):
 
 
 for num, (tensor, _) in enumerate(test_dataloader):
-    val_tensor = tensor.to(device).float()
-    num_known_points = 340
+    if num == 0:
+        val_tensor = tensor.to(device).float()
+        num_known_points = 340
 
-    if use_known_samples:
-        known_samples, known_mask = get_known_samples(val_tensor, num_known_points)
-        known_samples = resize(known_samples, (2, 64, 128)).to(device)
-        known_mask = resize(known_mask, (2, 64, 128)).to(device)
-    else:
-        known_samples = resize(val_tensor, (2, 64, 128)).to(device)
-        known_mask = torch.ones_like(known_samples).to(device)
+        if use_known_samples:
+            known_samples, known_mask = get_known_samples(val_tensor, num_known_points)
+            known_samples = resize(known_samples, (2, 64, 128)).to(device)
+            known_mask = resize(known_mask, (2, 64, 128)).to(device)
+        else:
+            known_samples = resize(val_tensor, (2, 64, 128)).to(device)
+            known_mask = torch.ones_like(known_samples).to(device)
 
 
-    land_mask = torch.zeros_like(known_samples).to(device)
+        land_mask = torch.zeros_like(known_samples).to(device)
 
-    land_mask[:, :, 10:20, 10:20] = 1
+        land_mask[:, :, 10:20, 10:20] = 1
 
-    noised_samples = generate_noised_tensor_iterative(known_samples, T, variance=0.005).float().to(device)
+        noised_samples = generate_noised_tensor_iterative(known_samples, T, variance=0.005).float().to(device)
 
-    print(f"Starting to sample {num}/{len(test_dataloader)} number of validation set")
-    samples = generate_samples(model, T, device, noised_samples, land_mask)
+        print(f"Starting to sample {num}/{len(test_dataloader)} number of validation set")
+        samples = generate_samples(model, T, device, noised_samples, known_mask)
 
-    generate_png(samples.cpu(), scale=9, output_path="./results", filename=f"output_{num}.png")
+        generate_png(samples.cpu(), scale=9, output_path="../../results", filename=f"ocean_output_{num}.png")
 
-    break
+        break
