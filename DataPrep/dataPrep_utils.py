7da0cc8fabf45f9c1ffd8f406efa9256bfdde138
Andrew Sims <simsd@plu.edu>
2025-06-02 16:01:11 -0700

    - Refactored and removed garbage.txt - Changed from standardizing to normalizing in train-ocean-xl.py

--- DataPrep/dataPrep_utils.py
+++
@@ -1,4 +0,0 @@
-def normalize_data(data_set, global_min, global_max):
-    margin_min = global_min - (global_min*.2)
-    margin_max = global_max + (global_max*.2)
-    return (data_set - margin_min) / (margin_max - margin_min)
--- garbage.txt/scripts/resize.py
+++
@@ -1,9 +0,0 @@
-from resize_tensor import resize
-
-class ResizeTransform:
-    def __init__(self, end_shape):
-        self.end_shape = end_shape
-
-    def __call__(self, tensor):
-        return resize(tensor, self.end_shape).float()
-
