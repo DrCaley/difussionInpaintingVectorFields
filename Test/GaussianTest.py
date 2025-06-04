b6aa0543510735ad5d0f6c4b60fbdec1e722dcf4
Andrew Sims <simsd@plu.edu>
2025-06-04 16:11:03 -0700

    added divergence computation tests

--- Test/GaussianTest.py
+++ Test/GaussianTest.py
@@ -1,4 +1,7 @@
 import unittest
+import torch
+from gaussian_process.incompressible_gp.adding_noise.divergence_free_noise import divergence_free_noise
+from gaussian_process.incompressible_gp.adding_noise.compute_divergence import compute_divergence
 
 from sympy.vector import divergence
 
@@ -7,10 +10,42 @@ class GaussianTest(unittest.TestCase):
     def test_test(self):
         self.assertTrue(False)
 
-    def test_divergence_tester(self):
+    def test_compute_divergence_vertical_direction(self):
+
+        tensor = torch.tensor(
+            [[
+            [[0., 0., 0., 0., 0., 0.],
+            [0., 0., 0., 0., 0., 0.],
+            [0., 0., 0., 0., 0., 0.],
+            [0., 0., 0., 0., 0., 0.],],
+            [[1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+             ],]])
 
 
+        divergence_tensor = compute_divergence(tensor[0][0], tensor[0][1])
+        divergence = divergence_tensor.abs().mean().item()
 
-        divergence
+        self.assertAlmostEqual(0, divergence, delta=0.01)
+
+    def test_compute_divergence_uniform_direction(self):
+
+        tensor = torch.tensor(
+            [[
+            [[1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],],
+            [[1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+            [1., 1., 1., 1., 1., 1.],
+             ],]])
+
+
+        divergence_tensor = compute_divergence(tensor[0][0], tensor[0][1])
+        divergence = divergence_tensor.abs().mean().item()
 
         self.assertAlmostEqual(0, divergence, delta=0.01)
