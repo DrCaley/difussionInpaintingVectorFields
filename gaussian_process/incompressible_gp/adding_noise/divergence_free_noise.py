e416e397370a5bbdc47e00cc2ff1dd2c0354aa57
Andrew Sims <simsd@plu.edu>
2025-06-04 16:39:09 -0700

    hope

--- gaussian_process/incompressible_gp/adding_noise/divergence_free_noise.py
+++ gaussian_process/incompressible_gp/adding_noise/divergence_free_noise.py
@@ -23,7 +23,7 @@ def exact_div_free_field_from_stream(H, W, freq, device='cpu'):
 def divergence_free_noise(data_set: torch.Tensor, t: torch.Tensor, device='cpu') -> torch.Tensor:
 
     betas = torch.linspace(0.0001, 0.02, 1000).to(device)
-    alphas = 1 - betas
+    alphas = (1 - betas).to(device)
     alpha_bars = torch.tensor([torch.prod(alphas[:i + 1]) for i in range(len(alphas))]).to(device)
 
     batch, _, height, width = data_set.shape
@@ -31,7 +31,7 @@ def divergence_free_noise(data_set: torch.Tensor, t: torch.Tensor, device='cpu')
 
     for i in range(batch):
         for j in range(t[i]):
-            freq = torch.normal(torch.tensor(0.0),std=alpha_bars[i].sqrt()).to(device)  # Not sure about this
+            freq = torch.normal(torch.tensor(0.0, device=device), std=alpha_bars[i].sqrt())  # Not sure about this
             vx, vy = exact_div_free_field_from_stream(width, height, freq, device=device)
             output[i, 0] += vx  # Accumulate vx
             output[i, 1] += vy  # Accumulate vy
