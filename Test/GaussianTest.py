fda598a2a58e68da60a41bc2cbc8baad2bc5123e
Andrew Sims <simsd@plu.edu>
2025-06-04 15:34:04 -0700

    requirements.txt :(

--- Test/GaussianTest.py
+++ Test/GaussianTest.py
@@ -1,8 +1,16 @@
 import unittest
 
+from sympy.vector import divergence
+
+
 class GaussianTest(unittest.TestCase):
     def test_test(self):
         self.assertTrue(False)
 
-    def flow_observation(self):
-        self.assertTrue(False)
+    def test_divergence_tester(self):
+
+
+
+        divergence
+
+        self.assertAlmostEqual(0, divergence, delta=0.01)
