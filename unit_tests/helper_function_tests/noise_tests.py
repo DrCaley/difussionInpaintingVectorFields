c2c51285f524daafd49b27d899d605e6c06deb8b
Andrew Sims <simsd@plu.edu>
2025-06-10 14:19:35 -0700

    mo tests

--- unit_tests/helper_function_tests/noise_tests.py
+++ unit_tests/helper_function_tests/noise_tests.py
@@ -1,7 +1,6 @@
 import unittest
 import torch
-from noising_process.incompressible_gp.adding_noise.divergence_free_noise import divergence_free_noise, \
-    normalized_divergence_free_noise, gaussian_at_end_divergence_free_noise, gaussian_each_step_divergence_free_noise
+from noising_process.incompressible_gp.adding_noise.divergence_free_noise import gaussian_each_step_divergence_free_noise
 from ddpm.helper_functions.compute_divergence import compute_divergence
 from plots.plot_data_tool import plot_vector_field
 
@@ -79,43 +78,10 @@ class GaussianTest(unittest.TestCase):
 
         self.assertAlmostEqual(0, divergence, delta=0.0)
 
-    def test_divergence_free_noise(self):
-
-        tensor = torch.zeros((2,2,64,128))
-        t = torch.randint(0, 1, (1000,))
-        noise = divergence_free_noise(tensor,t)
-
-        divergence_tensor = compute_divergence(noise[1][0], noise[1][1])
-        divergence = divergence_tensor.abs().mean().item()
-
-        plot_vector_field(noise[1][0], noise[1][1], 2, 1, title=f"Title")
-
-        self.assertAlmostEqual(5, divergence, delta=0.3)
-
-    def test_normalized_divergence_free_noise(self):
-        tensor = torch.zeros((50,2,3,3))
-        noise = normalized_divergence_free_noise(tensor, torch.randint(0,1000,(1000,)))
-
-        divergence_tensor = compute_divergence(noise[0][0], noise[0][1])
-        divergence = divergence_tensor.abs().mean().item()
-
-        self.assertAlmostEqual(0, divergence, delta=0.3)
-
-    def test_gaussian_at_end_vector_field(self):
-        tensor = torch.zeros((50, 2, 128, 128))
-        t = torch.randint(0, 1000, (1000,))
-        noise = gaussian_at_end_divergence_free_noise(tensor, t)
-        plot_vector_field(noise[0][0], noise[0][1],2,3, title=f"{t[0].item()}")
-
-        divergence_tensor = compute_divergence(noise[1][0], noise[1][1])
-        divergence = divergence_tensor.abs().mean().item()
-
-        self.assertAlmostEqual(5, divergence, delta=0.3)
-
     def test_gaussian_each_step(self):
-        tensor = torch.zeros((50, 2, 128, 128))
+        tensor = torch.zeros((1, 2, 128, 128))
         t = torch.randint(0, 1000, (1000,))
-        noise = gaussian_each_step_divergence_free_noise(tensor, t)
+        noise = gaussian_each_step_divergence_free_noise(tensor.shape, t)
         plot_vector_field(noise[0][0], noise[0][1],2,30, title=f"{t[0].item()}", file=f"{t[0].item()}.png")
 
         divergence_tensor = compute_divergence(noise[1][0], noise[1][1])
