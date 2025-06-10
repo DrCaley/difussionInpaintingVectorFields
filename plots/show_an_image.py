908043671c72169bbd5654a3c07464336a771168
MattHelm0r <matthew.helmer@plu.edu>
2025-06-09 17:02:51 -0700

    done!

--- plots/show_an_image.py
+++ plots/show_an_image.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import csv
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
@@ -11,12 +12,23 @@ from plot_data_tool import plot_vector_field
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './../')))
 from data_prep.ocean_image_dataset import OceanImageDataset
 from data_prep.data_initializer import DDInitializer
+from noising_process.incompressible_gp.adding_noise.compute_divergence import compute_divergence
 
 # Initialize data
 data_init = DDInitializer()
 
 # Directory to save images
 output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'outputs')
+
+# CSV
+csv_file = os.path.join(output_dir, f"divergences.csv")
+with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Number', 'Divergence'])
+
+# I've lost the plot
+plot_file = os.path.join(output_dir, f"div_plot.png")
+
 
 # Generate vector field images
 filenames = []
@@ -41,8 +53,10 @@ for ax in axs:
 
 
 
+divergences = []
+
 # IMAGE GENERATION LOOP
-for i in range(1000):
+for i in range(100):
     tensor_to_draw_x = data_init.training_tensor[:, :, 0, i+500]
     tensor_to_draw_y = data_init.training_tensor[:, :, 1, i+500]
 
@@ -62,13 +76,19 @@ for i in range(1000):
     plt.savefig(heatmap_file, dpi=150, bbox_inches='tight')
     heatmap_filenames.append(heatmap_file)
 
+    divergences.append(compute_divergence(tensor_to_draw_x, tensor_to_draw_y).nanmean().item())
+
+    with open(csv_file, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([i + 1, divergences[i]])
+
 
 # Create GIFS
 images = [imageio.imread(f) for f in sorted(filenames)]
-imageio.mimsave(output_dir + "\\vector_fields.gif", images, fps=50)  # Adjust fps as needed
+imageio.mimsave(output_dir + "\\vector_fields.gif", images, fps=10)  # Adjust fps as needed
 
 heat_images = [imageio.imread(f) for f in heatmap_filenames]
-imageio.mimsave(output_dir + "\\heat_map.gif", heat_images, fps=50)
+imageio.mimsave(output_dir + "\\heat_map.gif", heat_images, fps=10)
 
 
 # No more 1000+ image pushes :(
@@ -77,3 +97,13 @@ for f in filenames + heatmap_filenames:
         os.remove(f)
     except OSError as e:
         print(f"Error deleting file {f}: {e}")
+
+
+# Make the plot
+plt.figure(figsize=(20, 10))
+plt.plot(divergences, label='Divergence')
+plt.xlabel('Field')
+plt.ylabel('Divergence')
+plt.legend()
+plt.title('Fields and Divergences')
+plt.savefig(plot_file)
